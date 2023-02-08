c04061dbbfb5b2fceb1999fd4867febda3046ff6
Theawesometurtl <shablammmo@gmail.com>
2023-02-07 22:55:35 -0800

    Created main.py
    I copied some code to take bytes from the serial port and convert the bytes into readable material, then added some basic python scripting to write the data to another file. Hopefully, in the future, I will be able to send this data to a webserver, displaying it in a user-friendly manner

---
+++ main.py
@@ -0,0 +1,22 @@
+import serial
+import time
+ 
+ser = serial.Serial('COM4', 9800, timeout=1)
+time.sleep(2)
+f = open("Data.txt", "w+") 
+
+for i in range(50):
+    # Reading all bytes available bytes till EOL
+    line = ser.readline() 
+    if line:
+        # Converting Byte Strings into unicode strings
+        string = line.decode()  
+        # Converting Unicode String into integer
+        num = int(string) 
+        
+        f.write(i, "/n")
+        f.close()
+        print(num)
+ 
+ser.close()
+f.close()
